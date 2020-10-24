d26839efc3844e0770570c881f2d7c199b28f277
Emil Dymek <emil.dymek@gmail.com>
2020-10-25 00:21:30 +0200

    Audio Manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+
+    void Awake()
+    {
+        AudioMixer mixer = Resources.Load("AudioMixer1") as AudioMixer;
+
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.outputAudioMixerGroup = s.audioMixerGroup;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    void Start()
+    {
+        Play("Test Music");
+    }
+
+
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound " + name + " not found and could not be played!");
+            return;
+        }
+        s.source.Play();
+    }
+
+    public void Stop(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null)
+        {
+            Debug.LogWarning("Sound " + name + " not found and could not be stopped!");
+            return;
+        }
+        s.source.Stop();
+    }
+}
